b0ffe3668147f6670fc05423b92cbc0d2d030441
Amelia Ostrow <ameliaostrow@Amelias-MacBook-Air.local>
2022-06-12 12:57:23 -0400

    submission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profilepic from "../assets/profilepic.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left"> 
+          <div className="centered">
+           <img className="profile_image"
+           src={profilepic}
+           alt="Profile Pic"></img>
+          </div>
+        </div>
+        <div class="split right">
+          <div className="centered">
+            <div className="name_title"><h1>Amelia</h1></div>
+            <div className="brief description">
+              <p>
+                I am a rising junior at the University of Rochester
+                studying Brain & Cognitive Sciences and Linguistics.
+              </p>
+              <p>
+                I enjoy doing puzzles, playing tennis, and ice skating.
+                I'm also a huge fan of <em>Jeopardy!</em> and trivia
+                in general. (A former <em>Jeopardy!</em> champion
+                responded to me on Twitter once!)
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
